a160c2edb2aac9e16a941d8f5eb99490cfd1f6b2
Ehsan <eakhehsan@gmail.com>
2024-07-06 17:42:48 +0330

    some fixes

--- src/main/java/com/example/enums/LoginMenuCommands.java
+++ src/main/java/com/example/enums/LoginMenuCommands.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public enum LoginMenuCommands { // TODO - Sina - empty input fix
     Register("^user create -u (?<username>.+) -p (?<password>.+) (?<passwordConfirmation>.+) -email (?<email>.+) -n (?<nickname>.+)$"),
     RegisterRandom("^user create -u (?<username>.+) -p random -email (?<email>.+) -n (?<nickname>.+)$"),
-    QuestionPick("^question pick -q (?<questionNumber>.+) -a (?<answer>.+) -c (?<answerCnfirmation>.+)$"),
+    QuestionPick("^question pick -q (?<questionNumber>.+) -a (?<answer>.+) -c (?<answerConfirmation>.+)$"),
     Login("^user login -u (?<username>.+) -p (?<password>.+)$"),
     ForgetPassword("^Forgot my password -u (?<username>.+)$"),
     Exit("^Exit$");
@@ -22,7 +22,7 @@ public enum LoginMenuCommands { // TODO - Sina - empty input fix
 
     @Override
     public String toString() {
-        // TODO - ehsan - make it prettier
-        return pattern;
+        // TODO - DONE - ehsan - make it prettier
+        return RegexFormatter.regexToString(pattern) ;
     }
 }
