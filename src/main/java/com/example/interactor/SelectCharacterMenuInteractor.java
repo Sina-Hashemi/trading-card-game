d8f716850bc389635d8e09cdafdcd660331af77f
Sina Hashemi <hashemisina6@gmail.com>
2024-07-11 01:17:25 +0330

    ..

---
+++ src/main/java/com/example/interactor/SelectCharacterMenuInteractor.java
@@ -0,0 +1,35 @@
+package com.example.interactor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SelectCharacterMenuInteractor {
+    @FXML
+    protected Label playerName;
+    @FXML
+    private Button firstCharacterButton ;
+    @FXML
+    private Button secondCharacterButton ;
+    @FXML
+    private Button thirdCharacterButton ;
+    @FXML
+    private Button forthCharacterButton ;
+    @FXML
+    protected void onFirstButtonClick(){
+
+    }
+    @FXML
+    protected void onSecondButtonClick(){
+
+    }
+    @FXML
+    protected void onThirdButtonClick(){
+
+    }
+    @FXML
+    protected void onForthButtonClick(){
+
+    }
+
+}
